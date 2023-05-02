c4095aabb7e3710ca83f2759d0cc2db9fdd14961
Andreas MOUZONG <andreasmouzong@gmail.com>
2023-05-02 12:02:26 +0200

    Initial commit

---
+++ src/main/java/tech/codegrill/api/JavaCicdApiAppApplication.java
@@ -0,0 +1,14 @@
+package tech.codegrill.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCicdApiAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JavaCicdApiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/codegrill/api/JavaCicdApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.codegrill.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCicdApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
